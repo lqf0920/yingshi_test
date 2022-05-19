6358d7afd9286635093c980c2f4c9b01979d2463
Hoodie <1960612500@qq.com>
2022-05-19 14:49:50 +0800

    测试不同的请求方法和路径

--- src/main/java/com/example/yingshi_test/controller/YingshiController.java
+++ src/main/java/com/example/yingshi_test/controller/YingshiController.java
@@ -47,4 +47,15 @@ public class YingshiController {
         System.out.println("接收到参数为："+ param);
         return null;
     }
+
+    //测试不同的请求方法和路径
+    @GetMapping("/test3")  //http://localhost:8080/yingshi/test3?name=qq&age=11
+    public String test3(@RequestParam String name, @RequestParam String age){
+        return name + age;
+    }
+
+    @GetMapping("/test4/{name}/{age}")  // http://localhost:8080/yingshi/test4/qq/11
+    public String test4(@PathVariable String name, @PathVariable String age){
+        return name + age;
+    }
 }
